3f63ea34da0adb7e000e5a846240fe47218f8f1d
Hydrant <qbcbyb@sina.com>
2016-10-25 13:19:14 +0800

    add pano

---
+++ src/com/qbcbyb/xc/BaikeActivity.java
@@ -0,0 +1,32 @@
+package com.qbcbyb.xc;
+
+import android.content.DialogInterface;
+import android.os.Message;
+
+public class BaikeActivity extends ActivityBase {
+
+    @Override
+    protected void setLayout() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void addEventListener() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void handleMessage(Message arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void onCancelProgress(DialogInterface arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
